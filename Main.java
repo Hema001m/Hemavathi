9c2d0fc448d9e3f0a30f5834e37b423a92a6d906
Hema001m <106105851+Hema001m@users.noreply.github.com>
2022-05-25 15:56:32 +0530

    assign

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String args[]) {
+      String str="hello";
+      String str1="java";
+      System.out.println(str.length()+str1.length());
+      System.out.println(str.concat(str1));
+    }
+}
